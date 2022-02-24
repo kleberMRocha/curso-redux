2b7b49adfc2b3f54a6ccf23d81af6b948b969b55
kleber <kleberrocha2701@gmail.com>
2022-02-23 22:02:00 -0300

    Ajuste logica card

--- src/componets/Card.tsx
+++ src/componets/Card.tsx
@@ -1,54 +1,110 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import {FiThumbsUp, FiThumbsDown} from 'react-icons/fi';
+import { FiThumbsUp, FiThumbsDown,FiArrowLeft, FiArrowRight } from 'react-icons/fi';
 import './style.css';
-import { addPokemon, deslikePokemon,likePokesmon } from '../store/modules/pokemon/actions';
+import {
+  addPokemon,
+  deslikePokemon,
+  likePokesmon,
+} from '../store/modules/pokemon/actions';
 import { IPokemon } from '../store/modules/pokemon/reducer';
 import { IState } from '../store';
+import { api } from '../service';
+import { Loader } from './Loader';
 
-export const Card:React.FC = () => {
-    const pokemons = useSelector<IState, IPokemon[]>(state => state.pokemon);
-    const dispatch = useDispatch();
+export const Card: React.FC = () => {
+  const pokemons = useSelector<IState, IPokemon[]>((state) => state.pokemon);
 
-    const url = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png';
-    const mock = {
-        img:url, 
-        name: 'Bulbasaur', 
-        type: 'Grass,Poison', 
-        deslike: 0 , 
-        like: 0
-    };
+  console.log(pokemons);
 
-    const [pokemon, setPokeon] = useState(mock);
+  const dispatch = useDispatch();
+  const [pokemon, setPokeon] = useState({} as IPokemon);
+  const [numerPokemon, setNumber] = useState(1);
 
-    const handleAddPokemon = useCallback((pokeon:IPokemon, action: 'like' | 'deslike') => {
-        dispatch(addPokemon(pokeon));
-        action === 'like' 
-            ? dispatch(likePokesmon(pokeon)) 
-            : dispatch(deslikePokemon(pokeon));
-    },[dispatch]);
-    
+interface IPayLoadPoekon {
+    name: string;
+    types: { type: { name: string }}[];
+    sprites: {
+        front_default: string;
+    }
+}
 
-    const typeList = pokemon.type.split(',');
+  const formatPokeon = ({ name, types, sprites }:IPayLoadPoekon) => {
+    const pokeon: IPokemon = {
+        deslike: 0,
+        like: 0,
+        img: sprites.front_default,
+        name,
+        type: types
+          .map((t: { type: { name: string } }) => t.type.name)
+          .join(','),
+      };
 
-    return (<div className='card'>
-        <span><h2>{pokemon.name}</h2></span>
-        <img src={pokemon.img} />
-        <div className='card-type'>
+      return pokeon;
+  };
+
+  useEffect(() => {
+    api.get(`pokemon/${numerPokemon}`).then((response) => {
+      setPokeon(formatPokeon(response.data));
+    });
+  }, [numerPokemon]);
+
+  const handleAddPokemon = useCallback(
+    (pokeon: IPokemon, action: 'like' | 'deslike') => {
+        setNumber((old) => {
+            const next = old+=1;
+            return Number(next)
+        })
+      dispatch(addPokemon(pokeon));
+      action === 'like'
+        ? dispatch(likePokesmon(pokeon))
+        : dispatch(deslikePokemon(pokeon));
+    },
+    [dispatch]
+  );
+
+  return (
+    <div className="card">
+        <div className='selectorContainer'>
+            <button type='button'><FiArrowLeft/></button>
+            <button type='button'><FiArrowRight /></button>
+        </div>
+      {pokemon.name ? (
+        <>
+          <span>
+            <h2>{pokemon.name}</h2>
+          </span>
+          <img src={pokemon.img} />
+          <div className="card-type">
             {
-                typeList.map(t => <span key={t}>{t}</span>)
+                pokemon.type.split(',').map(t => {
+                    return <span key={t}>{t}</span>
+                })
             }
-        </div>
-        <div className='card-like-Container'>
-            <span><h4>Do you like this Pokemon? </h4></span>
-            <div className='card-btns'>
-            <button type='button' onClick={() => handleAddPokemon(mock, 'like')} >
-                <FiThumbsUp color='green' /> {pokemon.like}
-            </button>
-            <button type='button' onClick={() => handleAddPokemon(mock, 'deslike')}>
-                <FiThumbsDown color='tomato'  /> {pokemon.deslike}
-            </button>
+          </div>
+          <div className="card-like-Container">
+            <span>
+              <h4>Do you like this Pokemon? </h4>
+            </span>
+            <div className="card-btns">
+              <button
+                type="button"
+                onClick={() => handleAddPokemon(pokemon, 'like')}
+              >
+                <FiThumbsUp color="green" /> {pokemon.like}
+              </button>
+              <button
+                type="button"
+                onClick={() => handleAddPokemon(pokemon, 'deslike')}
+              >
+                <FiThumbsDown color="tomato" /> {pokemon.deslike}
+              </button>
             </div>
-        </div>
-    </div>);
+          </div>
+        </>
+      ) : (
+        <Loader />
+      )}
+    </div>
+  );
 };
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,10 @@
 import {createStore } from 'redux';
 import { IPokemon } from './modules/pokemon/reducer';
 import root from './modules/root';
-
+import {composeWithDevTools} from 'redux-devtools-extension';
 export interface IState {
     pokemon: IPokemon[]
 }
 
-export const store = createStore(root);
+export const store = createStore(root, composeWithDevTools());
 
